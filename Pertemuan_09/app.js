c8907fc46557076bc748112dba0d0fa87c1eb00d
AlifNM0323 <betchesanm14@gmail.com>
2023-12-05 09:39:26 +0700

    Praktikum09

---
+++ Pertemuan_09/app.js
@@ -0,0 +1,19 @@
+const { index, store, update, destroy} = require('./controller/fruitController.js')
+// const fruitController = require('./controller/fruitController.js');
+
+let main = () => {
+    console.log("Method Index - menampilkan Buah")
+    index()
+    console.log('-------------------------')
+    console.log("Method store - Menambahkan Buah Pisang")
+    store("Pisang")
+    console.log('-------------------------')
+    console.log("Method update - Update data 0 menjadi kelapa")
+    update(0,"Kelapa")
+    console.log('-------------------------')
+    console.log("Method destroy - Menghapus data 0")
+    destroy(0)
+    console.log('-------------------------')
+}
+
+main()
